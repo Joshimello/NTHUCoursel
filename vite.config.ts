9c43b3dabfe917564d7f0c9159b217860e0e79f8
Joshimello <joshualeanjw@gmail.com>
2023-01-04 23:56:49 +0800

    docs

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,4 @@ import WindiCSS from 'vite-plugin-windicss'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte(), WindiCSS()],
-  build: {
-    outDir: './docs'
-  }
 })
